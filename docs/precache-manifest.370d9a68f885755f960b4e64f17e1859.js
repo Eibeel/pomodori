abc1c19b1d41321726b4826b1a98a4a9650c7ed4
Abel Guardo <abelguardop@gmail.com>
2022-09-15 16:08:30 -0500

    refactor/docs

---
+++ docs/precache-manifest.370d9a68f885755f960b4e64f17e1859.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "240e2ff98c3c20fd50ed",
+    "url": "/static/js/main.240e2ff9.chunk.js"
+  },
+  {
+    "revision": "2c654603adbf035b3d92",
+    "url": "/static/js/1.2c654603.chunk.js"
+  },
+  {
+    "revision": "d06f1ab4adb7127ef1fa",
+    "url": "/static/js/2.d06f1ab4.chunk.js"
+  },
+  {
+    "revision": "fcfee1a720536df63a34",
+    "url": "/static/js/runtime~main.fcfee1a7.js"
+  },
+  {
+    "revision": "4b3a0363fcf30d63738782b7f452b7c9",
+    "url": "/static/media/timeOut.4b3a0363.mp3"
+  },
+  {
+    "revision": "fdab8ad2930178e5b7d3463c3c26512b",
+    "url": "/index.html"
+  }
+];
